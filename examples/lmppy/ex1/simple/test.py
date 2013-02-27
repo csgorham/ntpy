c922882c81f2606572bcdd4a81f1495add65d775
Jason Larkin <jasonlarkin84@gmail.com>
2013-02-27 16:06:30 -0500

    Working on lmppy

--- examples/lmppy/ex1/simple/test.py
+++ examples/lmppy/ex1/simple/test.py
@@ -15,6 +15,10 @@ velx = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
 vely = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
 velz = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
 
+# x = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
+x = np.zeros( (nmd.numTstep, 768,768), dtype=float)
+y = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
+z = np.zeros( (nmd.numTstep, nmd.numAtoms), dtype=float)
 
 ###--- MAIN ---###
 
@@ -45,31 +49,63 @@ print "Proc %d out of %d procs has" % (pypar.rank(),pypar.size()),lmp1
 
 # Not sure if works like this
 for istep in range(nmd.numTstep):
-	lmp1.command('run 32 pre no post no')
+	lmp1.command('run 32 pre no post yes')
  	velx[istep, :] = lmp1.extract_variable("vx", "all", 1)
  	vely[istep, :] = lmp1.extract_variable("vy", "all", 1)
  	velz[istep, :] = lmp1.extract_variable("vz", "all", 1)
+ 	# x = lmp1.gather_atoms("x", 1, 3)
+ 	x[istep, :,:] = lmp1.gather_atoms("x", 1, 3)
+	np.save('x.pos.npy', x)
+ 	y[istep, :] = lmp1.gather_atoms("y", 1, 3)
+ 	z[istep, :] = lmp1.gather_atoms("z", 1, 3)
+ 	# x = lmp1.extract_atom("x", 3)
+	# print x[0][0]
+	# print x[255][0]
+	# print x[256][0]
+	# print x[0][255]
+	# print x[0][256]
+	# print x[0][257]
+ 	# x[istep, :] = lmp1.extract_atom("x", 3)
+ 	# y[istep, :] = lmp1.extract_atom("y", 3)
+ 	# z[istep, :] = lmp1.extract_atom("z", 3)
+ 	# x[istep, :] = lmp1.extract_variable("x", "all", 1)
+ 	# y[istep, :] = lmp1.extract_variable("y", "all", 1)
+ 	# z[istep, :] = lmp1.extract_variable("z", "all", 1)
+	raw_input()
+	
 
+print x.shape
+print x
+print x[0,0]
 np.savetxt('vel.out.x.txt', velx[:,:], delimiter='\n')
 np.savetxt('vel.out.y.txt', vely[:,:], delimiter='\n')
 np.savetxt('vel.out.z.txt', velz[:,:], delimiter='\n')
 
-print velx[0,:]
-print velx[1,:]
-print velx[2,:]
+np.savetxt('pos.out.x.txt', x[:,:], delimiter='\n')
+np.savetxt('pos.out.y.txt', y[:,:], delimiter='\n')
+np.savetxt('pos.out.z.txt', z[:,:], delimiter='\n')
+
+# print velx[0,:]
+# print velx[1,:]
+# print velx[2,:]
 pypar.finalize() 
 print 'last line of python script'
 
 velx2, vely2, velz2 = norm.lmpReadBin('../simple2/out.vel.bin', nmd.numTstep, nmd.numAtoms)
+x2, y2, z2 = norm.lmpReadBin('../simple2/out.pos.bin', nmd.numTstep, nmd.numAtoms)
 
-print velx.shape
-print len(velx2)
-print vely.shape
-print len(vely2)
-print velz.shape
-print len(velz2)
+# print velx.shape
+# print len(velx2)
+# print vely.shape
+# print len(vely2)
+# print velz.shape
+# print len(velz2)
 
-np.savetxt('vel.out.x.diff.txt', velx[:1023,0] - velx2[1:,0], delimiter='\n')
-np.savetxt('vel.out.y.diff.txt', vely[:1023,0] - vely2[1:,0], delimiter='\n')
-np.savetxt('vel.out.z.diff.txt', velz[:1023,0] - velz2[1:,0], delimiter='\n')
+np.savetxt('vel.out.x.diff.txt', velx[:1023,1] - velx2[1:,1], delimiter='\n')
+np.savetxt('vel.out.y.diff.txt', vely[:1023,1] - vely2[1:,1], delimiter='\n')
+np.savetxt('vel.out.z.diff.txt', velz[:1023,1] - velz2[1:,1], delimiter='\n')
+
+np.savetxt('pos.out.x.diff.txt', x[:1023,1] - x2[1:,1], delimiter='\n')
+np.savetxt('pos.out.y.diff.txt', y[:1023,1] - y2[1:,1], delimiter='\n')
+np.savetxt('vel.out.z.diff.txt', z[:1023,1] - z2[1:,1], delimiter='\n')
 
