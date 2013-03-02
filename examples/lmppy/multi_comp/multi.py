92c8da095f2b63dd5f162851a47d2a4ab9761c0b
Kevin Parrish <kevin.dale.parrish@gmail.com>
2013-03-02 16:18:48 -0500

    Parallel lammps velocity pull working.
    Parallel lammps velocity pull now works. I find it helps to think as one of the proc reading the script.
    Maybe one area of impovement could be the loop to receive velocities

--- examples/lmppy/multi_comp/multi.py
+++ examples/lmppy/multi_comp/multi.py
@@ -13,22 +13,40 @@ import numpy as np
 ### Parameters ###
 numTsteps = 1024
 numAtoms = 256
+### IMPORTS ###
+import pypar
+import multiprocessing as mp
+from lammps import lammps
+import ctypes
+import numpy as np
+# import sys
+# sys.path.append('/home/kevin/projects/ntpy')
+# import ntpy.lattice as lt
+# import ntpy.param.lj as lj
+# import ntpy.nmd as norm
 
-# Create velocity Array
-#velx = np.zeros( (numTsteps, numAtoms), dtype=float)
-#vely = np.zeros( (numTsteps, numAtoms), dtype=float)
-#velz = np.zeros( (numTsteps, numAtoms), dtype=float)
-velx_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
-velx = np.ctypeslib.as_array(velx_base.get_obj())
-velx = velx.reshape(numTsteps, numAtoms)
-vely_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
-vely = np.ctypeslib.as_array(vely_base.get_obj())
-vely = vely.reshape(numTsteps, numAtoms)
-velz_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
-velz = np.ctypeslib.as_array(velz_base.get_obj())
-velz = velz.reshape(numTsteps, numAtoms)
+### Parameters ###
+proc = pypar.size()
+myid = pypar.rank()
 
-assert velx.base.base is velx_base.get_obj()
+numTsteps = 1024
+numAtoms = 256
+
+# Number of atoms calculated on by each lmp processs
+assert 256 % pypar.size() == 0
+numA2 = 256 / pypar.size()
+
+## Create velocity Arrays
+# If main process
+if myid == 0:
+	velx = np.zeros( (numTsteps, numAtoms), dtype=float)
+	vely = np.zeros( (numTsteps, numAtoms), dtype=float)
+	velz = np.zeros( (numTsteps, numAtoms), dtype=float)
+# Else, if not main process
+else:
+	tmpVelx = np.zeros( (numA2), dtype=float)
+	tmpVely = np.zeros( (numA2), dtype=float)
+	tmpVelz = np.zeros( (numA2), dtype=float)
 
 
 ###--- MAIN ---###
@@ -39,13 +57,36 @@ print "Proc %d out of %d procs has" % (pypar.rank(),pypar.size()),lmp1
 # Lammps run
 for istep in range(numTsteps):
 	lmp1.command('run 32 pre no post yes')
- 	velx[istep, 64*pypar.rank():64*(pypar.rank()+1)] = lmp1.extract_variable("vx", "all", 1)
- 	vely[istep, 64*pypar.rank():64*(pypar.rank()+1)] = lmp1.extract_variable("vy", "all", 1)
- 	velz[istep, 64*pypar.rank():64*(pypar.rank()+1)] = lmp1.extract_variable("vz", "all", 1)	
+	# If main process
+	if myid == 0:
+		# First get the velocities associated with this process
+ 		velx[istep, 0:numA2*(1)] = lmp1.extract_variable("vx", "all", 1)
+ 		vely[istep, 0:numA2*(1)] = lmp1.extract_variable("vy", "all", 1)
+ 		velz[istep, 0:numA2*(1)] = lmp1.extract_variable("vz", "all", 1)	
+		# Next, collect the velocities from all the other processes
+		for i in range(1, proc):
+			velx[istep, numA2*i:numA2*(i+1)] = pypar.receive(source=i, buffer=velx[istep,numA2*i:numA2*(i+1)], tag=1)
+			vely[istep, numA2*i:numA2*(i+1)] = pypar.receive(source=i, buffer=vely[istep,numA2*i:numA2*(i+1)], tag=2)
+			velz[istep, numA2*i:numA2*(i+1)] = pypar.receive(source=i, buffer=velz[istep,numA2*i:numA2*(i+1)], tag=3)
 
-print velx
-print vely
-print velz
+	# Else, if not the main process
+	else:
+		# Run and send the velocity information back to proc 0
+		tmpVelx[:] = lmp1.extract_variable("vx", "all", 1)
+		tmpVely[:] = lmp1.extract_variable("vy", "all", 1)
+		tmpVelz[:] = lmp1.extract_variable("vz", "all", 1)
+		pypar.send(tmpVelx, destination=0, use_buffer=True, tag=1)
+		pypar.send(tmpVely, destination=0, use_buffer=True, tag=2)
+		pypar.send(tmpVelz, destination=0, use_buffer=True, tag=3)
+
+if myid == 0:
+	print velx
+	print vely
+	print velz
+
+	np.savetxt('out.vel.x.txt', velx, delimiter='\n')
+	np.savetxt('out.vel.y.txt', vely, delimiter='\n')
+	np.savetxt('out.vel.z.txt', velz, delimiter='\n')
 
 # START MP
 #shared_array_base = mp.Array(ctypes.c_double, 10*10)
@@ -66,3 +107,20 @@ print velz
 # FINISH PYPAR
 pypar.finalize() 
 print 'last line of python script'
+# Create velocity Array
+#velx = np.zeros( (numTsteps, numAtoms), dtype=float)
+#vely = np.zeros( (numTsteps, numAtoms), dtype=float)
+#velz = np.zeros( (numTsteps, numAtoms), dtype=float)
+#velx_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
+#velx = np.ctypeslib.as_array(velx_base.get_obj())
+#velx = velx.reshape(numTsteps, numAtoms)
+#vely_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
+#vely = np.ctypeslib.as_array(vely_base.get_obj())
+#vely = vely.reshape(numTsteps, numAtoms)
+#velz_base = mp.Array(ctypes.c_double, numTsteps*numAtoms)
+#velz = np.ctypeslib.as_array(velz_base.get_obj())
+#velz = velz.reshape(numTsteps, numAtoms)
+
+#assert velx.base.base is velx_base.get_obj()
+
+
